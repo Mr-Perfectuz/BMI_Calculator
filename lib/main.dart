eddb4f8076f43f217021b905e774e2d308b55740
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-21 08:31:55 +0500

    Project has been updated

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       home: InputPage(),
     );
   }
