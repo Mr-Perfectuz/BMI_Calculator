8623bb8e49cab5e8e850164879bba2c11647dec1
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-21 10:20:33 +0500

    Rows and columns have been updated

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -13,59 +13,33 @@ class _InputPageState extends State<InputPage> {
         title: Center(child: Text('BMI CALCULATOR')),
       ),
       body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                margin: EdgeInsets.all((15.0)),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                height: 170.0,
-                width: 160.0,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all((15.0)),
+              decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10.0),
               ),
-              Container(
-                margin: EdgeInsets.all((15.0)),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                height: 170.0,
-                width: 160.0,
-              ),
-            ],
-          ),
-          Container(
-            margin: EdgeInsets.all((15.0)),
-            decoration: BoxDecoration(
-              color: Color(0xFF1D1E33),
-              borderRadius: BorderRadius.circular(10.0),
             ),
-            height: 170.0,
-            width: 360.0,
           ),
-          Row(
-            children: [
-              Container(
-                margin: EdgeInsets.all((15.0)),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                height: 170.0,
-                width: 160.0,
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all((15.0)),
+              decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10.0),
               ),
-              Container(
-                margin: EdgeInsets.all((15.0)),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                height: 170.0,
-                width: 160.0,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all((15.0)),
+              decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10.0),
               ),
-            ],
+            ),
           ),
         ],
       ),
