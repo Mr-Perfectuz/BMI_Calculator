3edca312bfbf0cbebda57e1c2a34e4bcaaf284c6
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-21 08:54:23 +0500

    Project has been updated

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class BMICalculator extends StatelessWidget {
       theme: ThemeData(
           primaryColor: Color(0xFF0A0E21),
           accentColor: Colors.purple,
-          backgroundColor: Colors.red),
+          scaffoldBackgroundColor: Colors.blueGrey),
       home: InputPage(),
     );
   }
