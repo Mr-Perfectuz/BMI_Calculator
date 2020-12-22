150a6604048b5ce7e9ed36a98b6eff675665c966
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-22 12:29:37 +0500

    Custom widget has been updated

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -70,11 +70,13 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
+  ReusableCard({@required this.colour, this.carchild});
   final Color colour;
+  final Widget carchild;
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: carchild,
       margin: EdgeInsets.all((15.0)),
       decoration: BoxDecoration(
         color: colour,
