1cef3f8ab570dbfdd26a929507833950634b39e9
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-23 16:41:13 +0500

    ternary operator has been implemented

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -6,12 +6,14 @@ class ReusableCard extends StatelessWidget {
   final Widget carchild;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: carchild,
-      margin: EdgeInsets.all((15.0)),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      child: Container(
+        child: carchild,
+        margin: EdgeInsets.all((15.0)),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }
