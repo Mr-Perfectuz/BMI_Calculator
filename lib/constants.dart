8731d5abdf596138ac2103626c53e310caf4103e
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-24 13:08:07 +0500

    Result Page has been updated

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+const KbottomContainerHeigh = 50.0;
+const KactiveCardColor = Color(0xFF1D1E33);
+const KbottomContainerColor = Color(0xFFEB1555);
+const KincativeCardColor = Color(0xFF111328);
+const KSliderActiveColor = Colors.white;
+const KSliderInactiveColor = Color(0xFF8D8E98);
+
 const KlabelTextStyle = TextStyle(
   fontSize: 30.0,
   color: Color(0xFF8D8E98),
@@ -23,11 +30,4 @@ const KWeightTextStyle = TextStyle(
 const KTitleTextStyle = TextStyle(
   fontWeight: FontWeight.bold,
   fontSize: 50.0,
-),
-
-const KbottomContainerHeigh = 50.0;
-const KactiveCardColor = Color(0xFF1D1E33);
-const KbottomContainerColor = Color(0xFFEB1555);
-const KincativeCardColor = Color(0xFF111328);
-const KSliderActiveColor = Colors.white;
-const KSliderInactiveColor = Color(0xFF8D8E98);
+);
--- lib/result_Page.dart
+++ lib/result_Page.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
 
 import 'constants.dart';
+import 'reusable_card.dart';
 
 class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text(
-          "BMI CALCULATOR",
-        ),
+        title: Center(child: Text("BMI CALCULATOR")),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Container(
-            child: Text(
-              'Your Result',
-              style: KTitleTextStyle,
+          Expanded(
+            child: Container(
+              child: Text(
+                'Your Result',
+                style: KTitleTextStyle,
+              ),
             ),
-          )
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: KactiveCardColor,
+            ),
+          ),
         ],
       ),
     );
