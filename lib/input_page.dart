323125e84ca86181ac412d57b8d5d64e02953ec2
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-24 08:48:36 +0500

    SliderTheme has been adjusted

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -89,13 +89,17 @@ class _InputPageState extends State<InputPage> {
                   ),
                   SliderTheme(
                     data: SliderTheme.of(context).copyWith(
-                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15),
-                    ),
+                        activeTrackColor: KSliderActiveColor,
+                        thumbColor: Color(0xFFEB1555),
+                        overlayColor: Color(0x29EB1555),
+                        thumbShape:
+                            RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        overlayShape:
+                            RoundSliderOverlayShape(overlayRadius: 30.0)),
                     child: Slider(
                       value: height.toDouble(),
                       min: 120.0,
                       max: 250.0,
-                      activeColor: KSliderActiveColor,
                       inactiveColor: KSliderInactiveColor,
                       onChanged: (double newValue) {
                         setState(() {
