71e4174f72fc1baf30b24e1099849a9d2b1b5ecb
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-28 14:11:05 +0500

    Age and Weight  Page has been updated

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -226,7 +226,7 @@ class RoundIconButton extends StatelessWidget {
       child: Icon(icon),
       elevation: 6.0,
       constraints: BoxConstraints.tightFor(width: 56.0, height: 56.0),
-      shape: CircleBorder(),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       fillColor: Color(0xFF4C4F5E),
       onPressed: () {},
     );
