15188b37ef55e6c36c79203fad7e9bfecddd4908
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-29 16:54:50 +0500

    result_Page.dart has been updated

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -5,8 +5,20 @@ class CalculatorBrain {
   final int height;
   final int weight;
 
+  double _bmi;
+
   String calculateBMI() {
-    double bmi = weight / pow(height / 100, 2);
-    return bmi.toStringAsFixed(1);
+    double _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
   }
 }
